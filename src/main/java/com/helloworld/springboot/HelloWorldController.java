ea99d6d8669e14b69d2d191ddcb572ab26c150bc
sow0731 <sow.ishihara@gmail.com>
2022-05-06 23:09:06 +0900

    first commit

---
+++ src/main/java/com/helloworld/springboot/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.helloworld.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("helloworld")
+
+    public String hello (ModelMap modelMap) {
+        modelMap.addAttribute ("message", "HelloWorld");
+        modelMap.addAttribute ("time", LocalDateTime.now ().toString ());
+        return "helloworld";
+    }
+}
